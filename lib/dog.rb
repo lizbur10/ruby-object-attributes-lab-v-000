8d50e959cbb16ad0b4509f8fa64eb722ba292433
Liz Burton <liz.burton147@gmail.com>
2017-12-02 13:55:00 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Dog
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+
+  def name
+    @this_dogs_name
+  end
+end
+fido = Dog.new
+fido.name ="Fido"
+
+puts fido.name
+# binding.pry
+
+snoopy = Dog.new
+snoopy.name = "Snoopy"
+puts snoopy.name
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
